c3ae06b3a95246074cc52ac0f8b77020b770a14a
Schuyler Zheng <schuylerzheng@gmail.com>
2025-06-10 10:29:01 -0700

    removed the vstools because the newer 2.7 is here (you have to install it in marketplace folder because vs fucked it up). Also made a new class for the landscape, requiring me to add more modules

---
+++ Source/Miner/Public/WorldLandscape.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DynamicMeshActor.h"
+#include "WorldLandscape.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINER_API AWorldLandscape : public ADynamicMeshActor
+{
+	GENERATED_BODY()
+	
+};
