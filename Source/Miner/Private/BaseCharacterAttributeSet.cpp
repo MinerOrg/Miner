63b642a687ee4136675b208fcf9d4f412c09089e
Schuyler Zheng <schuylerzheng@gmail.com>
2025-07-08 18:21:02 -0700

    Crouching and Some Other Changes
    * Crouching support
    * Mutable plugins enabled
    * Reorganized PCG assets in LvL_Test
    * Attribute Clamping

--- Source/Miner/Private/BaseCharacterAttributeSet.cpp
+++ Source/Miner/Private/BaseCharacterAttributeSet.cpp
@@ -3,4 +3,23 @@
 
 #include "BaseCharacterAttributeSet.h"
 
+void UBaseCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
 
+	ClampValues(Attribute, NewValue);
+}
+
+void UBaseCharacterAttributeSet::ClampValues(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	// Clamp the health value to be between 0 and MaxHealth
+	if (Attribute == GetHealthAttribute()) {
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+		return;
+	}
+
+	// Clamp the stamina value to be between 0 and MaxStamina
+	if (Attribute == GetStaminaAttribute()) {
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxStamina());
+	}
+}
