26ab1014fdb47b298ad34c0ba458ed991e2e8676
Schuyler Zheng <schuylerzheng@gmail.com>
2026-01-03 15:37:48 -0800

    Landscape: Added Verticies and Triangles in frunnable
    * Added Verticies and Triangles in Frunnable then, set it to the actual one in edit mesh
    * Still slow.
    * Could mix octree dynamic mesh component with the current one (current base one has collision the octree version can change seprate parts of the mesh)
    * Could be a problem with the game thread still waiting for the other thread to finish
    * Going to do a little bit more testing, then if nothing works, I will just give up for now

--- Source/Miner/Private/WorldLandscape.cpp
+++ Source/Miner/Private/WorldLandscape.cpp
@@ -52,16 +52,18 @@ void AWorldLandscape::BeginPlay()
 	DynamicMesh = AllocateComputeMesh();
 	DynamicMeshComponent->SetDynamicMesh(DynamicMesh);
 
-	SetupNoise();
-
 	// Create the thread that generates the vertex locations
 	WorldGenerationRunnable = new FWorldGenerationRunnable(this, GetWorld());
 
+	// Pre-allocate vertex locations array
 	const int NumPointsPerLine = FMath::FloorToInt((RenderDistance * 2.0f) / Resolution) + 1;
 	int64 ReserveCount = (int64)NumPointsPerLine * (int64)NumPointsPerLine;
 	if (ReserveCount > TNumericLimits<int32>::Max()) ReserveCount = TNumericLimits<int32>::Max();
 	GeneratedVertexLocations.Reserve((int32)ReserveCount);
 
+	SetupNoise();
+
+	// Generate the mesh (last step)
 	GenerateVertexLocations();
 }
 
@@ -128,12 +130,9 @@ void AWorldLandscape::GenerateTerrain()
 	checkf(IsValid(DynamicMesh), TEXT("Dynamic Mesh was bad"));
 	checkf(Noise, TEXT("Noise was bad"));
 	
-	DynamicMesh->InitializeMesh();
-
 	// EditMesh > just using notifymesh because more safe
 	DynamicMesh->EditMesh([&](UE::Geometry::FDynamicMesh3& Mesh) {
-		ApplyGeneratedMeshData(Mesh);
-		PostGeneration(Mesh);
+		Mesh = *DynamicMesh->GetMeshPtr();
 
 		// Final validity checks
 		ensureMsgf(Mesh.CheckValidity(ValidityOptions, ValidityCheckFailMode), TEXT("Mesh was not valid"));
@@ -143,52 +142,7 @@ void AWorldLandscape::GenerateTerrain()
 
 void AWorldLandscape::ApplyGeneratedMeshData(UE::Geometry::FDynamicMesh3& Mesh)
 {
-	// Won't generate in a non-game thing because dynamicmesh isn't initialized
-	if (GetWorld()->IsGameWorld()) { const FVector3d LocalClientPawnLocation = LocalClientPawn->GetActorLocation(); }
-	else { const FVector3d LocalClientPawnLocation = FVector3d(0, 0, 0); }
-	const int NumPointsPerLine = FMath::FloorToInt((RenderDistance * 2.0f) / Resolution) + 1;
-
-	TArray<int32> Verticies;
-	int64 ReserveCount = (int64)NumPointsPerLine * (int64)NumPointsPerLine;
-	if (ReserveCount > TNumericLimits<int32>::Max()) ReserveCount = TNumericLimits<int32>::Max();
-	Verticies.Reserve((int32)ReserveCount);
-
-	// apply generated verticies
-	int32 Index = 0;
-	for (int IndexY = 0; IndexY < NumPointsPerLine; ++IndexY) {
-		float VertexY = -RenderDistance + IndexY * Resolution;
-
-		for (int IndexX = 0; IndexX < NumPointsPerLine; ++IndexX) {
-			float VertexX = -RenderDistance + IndexX * Resolution;
-
-			// create vertex and remember its index
-			int32 NewVertex = Mesh.AppendVertex(GeneratedVertexLocations[Index]);
-			UE_LOG(LogLandscape, Log, TEXT("Location: %s"), *GeneratedVertexLocations[0].ToString());
-			Index++;
-			check(Mesh.IsVertex(NewVertex));
-			Verticies.Add(NewVertex);
-		}
-	}
 	
-	// Clear the array for it to be generated again
-	GeneratedVertexLocations.Empty();
-
-	// Create the triangles
-	for (int IndexY = 0; IndexY < NumPointsPerLine - 1; ++IndexY) {
-		for (int IndexX = 0; IndexX < NumPointsPerLine - 1; ++IndexX) {
-			// Apparently making them into individual variables instead of an array is better
-			const int TopLeftVertex = Verticies[IndexX + IndexY * NumPointsPerLine];             // top left
-			const int TopRightVertex = Verticies[(IndexX + 1) + IndexY * NumPointsPerLine];       // top right
-			const int BottomLeftVertex = Verticies[IndexX + (IndexY + 1) * NumPointsPerLine];       // bottom left
-			const int BottomRightVertex = Verticies[(IndexX + 1) + (IndexY + 1) * NumPointsPerLine]; // bottom right
-
-			// First triangle (top-left, bottom-left, bottom-right)
-			Mesh.AppendTriangle(TopLeftVertex, BottomLeftVertex, BottomRightVertex);
-
-			// Second triangle (top-left, bottom-right, top-right)
-			Mesh.AppendTriangle(TopLeftVertex, BottomRightVertex, TopRightVertex);
-		}
-	}
 }
 
 void AWorldLandscape::PostGeneration(UE::Geometry::FDynamicMesh3& Mesh)
@@ -200,8 +154,16 @@ void AWorldLandscape::GenerateVertexLocations()
 {
 	TRACE_CPUPROFILER_EVENT_SCOPE(GenerateVertexLocations);
 
-	const FVector LocalClientPawnLocation = LocalClientPawn->GetActorLocation();
+	DynamicMesh->InitializeMesh();
+
+	// Won't generate in a non-game thing because dynamicmesh isn't initialized, but it is here just incase I add it in the future
+	const FVector3d LocalClientPawnLocation = (GetWorld()->IsGameWorld()) ? LocalClientPawn->GetActorLocation() : FVector3d::ZeroVector;
 	const int NumPointsPerLine = FMath::FloorToInt((RenderDistance * 2.0f) / Resolution) + 1;
+
+	TArray<int32> Verticies;
+	int64 ReserveCount = (int64)NumPointsPerLine * (int64)NumPointsPerLine;
+	if (ReserveCount > TNumericLimits<int32>::Max()) ReserveCount = TNumericLimits<int32>::Max();
+	Verticies.Reserve((int32)ReserveCount);
 
 	// create vertices in row-major order: x changes fastest (this makes it faster?)
 	for (int IndexY = 0; IndexY < NumPointsPerLine; ++IndexY) {
@@ -214,8 +176,26 @@ void AWorldLandscape::GenerateVertexLocations()
 			float Height = Noise->GetNoise(VertexX + LocalClientPawnLocation.X / 50, VertexY + LocalClientPawnLocation.Y / 50) * HeightScale;
 
 			// create vertex and remember its index
-			FVector3d NewVertex = FVector3d(VertexX + LocalClientPawnLocation.X / 50, VertexY + LocalClientPawnLocation.Y / 50, Height);
-			GeneratedVertexLocations.Add(NewVertex);
+			int32 NewVertex = DynamicMesh->GetMeshPtr()->AppendVertex(FVector3d(VertexX + LocalClientPawnLocation.X / 50, VertexY + LocalClientPawnLocation.Y / 50, Height));
+			check(DynamicMesh->GetMeshPtr()->IsVertex(NewVertex));
+			Verticies.Add(NewVertex);
+		}
+	}
+
+	// Create the triangles
+	for (int IndexY = 0; IndexY < NumPointsPerLine - 1; ++IndexY) {
+		for (int IndexX = 0; IndexX < NumPointsPerLine - 1; ++IndexX) {
+			// Apparently making them into individual variables instead of an array is better
+			const int TopLeftVertex = Verticies[IndexX + IndexY * NumPointsPerLine];             // top left
+			const int TopRightVertex = Verticies[(IndexX + 1) + IndexY * NumPointsPerLine];       // top right
+			const int BottomLeftVertex = Verticies[IndexX + (IndexY + 1) * NumPointsPerLine];       // bottom left
+			const int BottomRightVertex = Verticies[(IndexX + 1) + (IndexY + 1) * NumPointsPerLine]; // bottom right
+
+			// First triangle (top-left, bottom-left, bottom-right)
+			DynamicMesh->GetMeshPtr()->AppendTriangle(TopLeftVertex, BottomLeftVertex, BottomRightVertex);
+
+			// Second triangle (top-left, bottom-right, top-right)
+			DynamicMesh->GetMeshPtr()->AppendTriangle(TopLeftVertex, BottomRightVertex, TopRightVertex);
 		}
 	}
 
